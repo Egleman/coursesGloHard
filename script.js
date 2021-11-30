60e490dde5d9362bea070eae79d7ad7e4cda265d
Egleman <martsnov@hotmail.com>
2021-11-30 11:55:10 +0300

    Выполнил усложнённое 4 задание

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 
+const apple = (txt) => {
+    if (typeof txt !== 'string') {
+        return 'Не строка';
+    }
+    let text = txt.trim();
+    if (text.length > 30) {
+        return text.slice(0, 30) + '...';
+     } else {
+         return text;
+     }
+
+    
+    
+};
+
+console.log(apple('   Lorem ipsum dolor sit amet consec    '));
