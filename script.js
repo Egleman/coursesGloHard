5ee5b4035d9912e341437f7fed077d612a90fb80
Egleman <martsnov@hotmail.com>
2021-12-03 17:32:14 +0300

    Выполннил усложнённое 7 задание

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 
+const week = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
+
+const day = new Date();
+let numWeekDay = day.getDay();
+if ( numWeekDay === 0) { numWeekDay = 6; } else { numWeekDay--; }
+
+week.forEach((day, i) => {
+    let str = day;
+    if (i === numWeekDay) {
+        str = `<b>${day}</b>`;
+    } else {
+        str = `${day}`;
+    }
+    if (i === 5 || i === 6) { str = `<i>${str}</i>`; }
+    document.body.insertAdjacentHTML('beforeend', `<div>${str}</div>`);
+});
