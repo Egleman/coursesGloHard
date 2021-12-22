b8e77e16a2e36e751277da00945671cb9d221ee3
Egleman <martsnov@hotmail.com>
2021-12-22 16:57:09 +0300

    Выполнение второго усложнённого задания 19

--- script.js
+++ script.js
@@ -1,1 +1,87 @@
 
+let paragraph = document.querySelector('p');
+let debouncedOnInput = debounce(onInput);
+
+//Переменные для анимации
+let block = document.querySelector('.block');
+let end = 0;
+let step = 5;
+let isPause = false;
+let btnStart = document.querySelectorAll('button')[0];
+let reset = document.querySelectorAll('button')[1];
+let moveInterval = 0;
+
+
+
+function debounce(callback) {
+   let timeout;
+   return function (argument) {
+      clearTimeout(timeout);
+      timeout = setTimeout(callback, 300, argument);
+   };
+}
+
+function onInput(event) {
+   paragraph.textContent = event.target.value;
+}
+
+document.querySelector('input').addEventListener('keyup', debouncedOnInput);
+/*
+       
+             function move() {
+               end += step;
+               if(end == 500 || end == 0) {
+                   step = -step;
+               }    
+               block.style.marginLeft = end + 'px';
+               requestAnimationFrame(move);
+             }
+             requestAnimationFrame(move);
+           //document.addEventListener('keydown', function(event) {
+           //if(event.key == 'Enter') alert(end);  });
+
+
+    function move() {
+        end += step;
+        if (end == 500 || end == 0) {
+            step = -step;
+        }
+        block.style.marginLeft = step + 'px';
+        requestAnimationFrame(move);
+    }
+    requestAnimationFrame(move);
+
+
+*/
+function move() {
+    end += step;
+    if (end == 500 || end == 0) {
+        step = -step;
+    } else if (end < 0) {
+        step = 0;
+    }
+    block.style.marginLeft = end + 'px';
+    if(isPause === true) {
+        moveInterval = requestAnimationFrame(move);  
+    }
+}
+function pause() {
+    if(isPause === true) {
+        moveInterval = requestAnimationFrame(move);
+        btnStart.textContent = "Пауза";
+    }
+}
+
+
+btnStart.addEventListener('click', () => {
+    isPause = !isPause;
+    step = 5;
+    btnStart.textContent = 'Старт';
+    pause();
+});
+reset.addEventListener('click', () => {
+    btnStart.click();
+    end = 0;
+    block.style.marginLeft = end + 'px';
+    cancelAnimationFrame(moveInterval);
+});
